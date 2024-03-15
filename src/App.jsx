0de2f97a9100e812d3bf56717fdb6023ef2b745a
Srabosthy23e <srabosthycse@gmail.com>
2024-03-15 21:17:51 +0600

    initial task done and navbar section added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./assets/components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <div className="container mx-auto mt-5">
+            <div className="flex justify-between items-center bg-base-100">
+                <div className="">
+                    <a className="text-2xl font-bold">Kitchen Adventures</a>
+                </div>
+                <div className="space-x-10">
+                    <a>Home</a>
+                    <a>Recipies</a>
+                    <a>About</a>
+                    <a>Search</a>
+                </div>
+                <div className="flex gap-3 items-center">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full" />
+
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
